d1193a77efb0d2fdfbe758b14e5b86d2ceafafed
Adam Tovatt <sporedenstore@hotmail.com>
2024-12-16 20:10:20 +0100

    add code for removing background

---
+++ ImageCreatorApi/Managers/RemoveBackgroundManager.cs
@@ -0,0 +1,42 @@
+﻿namespace ImageCreatorApi.Managers
+{
+    public class RemoveBackgroundManager : IDisposable
+    {
+        public IFileManager fileManager;
+        private HttpClient client;
+        private string apiKey;
+
+        public RemoveBackgroundManager(IFileManager fileManager, string apiKey)
+        {
+            this.fileManager = fileManager;
+            this.apiKey = apiKey;
+            client = new HttpClient();
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+
+        public async Task<byte[]> RemoveBackgroundAsync(byte[] image)
+        {
+            using (MultipartFormDataContent formData = new MultipartFormDataContent())
+            {
+                formData.Headers.Add("X-Api-Key", apiKey);
+                formData.Add(new ByteArrayContent(image), "image_file", "file.jpg");
+                formData.Add(new StringContent("auto"), "size");
+                HttpResponseMessage response = await client.PostAsync("https://api.remove.bg/v1.0/removebg", formData);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsByteArrayAsync();
+                }
+                else
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Error when removing background from image: {errorMessage}");
+                }
+            }
+        }
+    }
+}
