5bfcd4e698b8d51b1437bc89ed273b4041825622
Adam Tovatt <sporedenstore@hotmail.com>
2024-12-16 19:51:22 +0100

    add file managers

---
+++ ImageCreatorApi/Managers/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageCreatorApi.Managers
+{
+    public interface IFileManager
+    {
+        public Task SaveAsync(byte[] bytes, string path);
+        public Task<byte[]> ReadSingleAsync(string path);
+        public Task<List<byte[]>> ReadAllAtPathAsync(string path);
+    }
+}
